a8290c6bd63f715644e340cefcd78d84132c5a70
Juan Edi <jedi@manas.com.ar>
2016-07-06 10:46:50 -0300

    Refactor

--- src/Snake/GridView.elm
+++ src/Snake/GridView.elm
@@ -4,17 +4,43 @@ import Snake exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Color exposing (..)
+import String exposing (concat)
 
--- VIEW
 
 cellWidth  = 28
 cellHeight = 28
 
-cellRgb : Model -> Int -> Int -> String
-cellRgb model x y = let
-                      rgb = Color.toRgb <| cellColor model x y
-                    in
-                      "rgb(" ++ toString rgb.red ++ "," ++ toString rgb.green ++ "," ++ toString rgb.blue ++ ")"
+
+-- STYLES
+
+px : Int -> String
+px n = (toString n) ++ "px"
+
+gridStyle : List((String, String))
+gridStyle = let gridWidth = Snake.columnCount * cellHeight
+            in [ ("width", px gridWidth)
+               , ("padding", "0")
+               ]  
+
+rowStyle : List((String, String))
+rowStyle = [ ("margin", "0")
+           , ("padding", "0")
+           ]
+
+cellStyle : List((String, String)) 
+cellStyle = [ ("float", "left")
+            , ("position", "relative")
+            , ("height", px cellHeight)
+            , ("width", px cellWidth)
+            ]
+
+rgb : Color -> String
+rgb color = let
+              {red, green, blue} = Color.toRgb color
+            in
+              String.concat [ "rgb(" , toString red , "," , toString green , "," , toString blue, ")" ]
+
+-- VIEW
 
 cellColor : Model -> Int -> Int -> Color
 cellColor model x y = if model.lost
@@ -26,39 +52,21 @@ cellColor model x y = if model.lost
                             then Color.red
                             else Color.grey
 
-px : Int -> String
-px n = (toString n) ++ "px"
-
 cell : Model -> Int -> Int -> Html a
-cell model rowIndex colIndex = let c = 1
+cell model rowIndex colIndex = let
+                                 cellRgb = rgb (cellColor model colIndex rowIndex)
+                                 styles = ("background-color", cellRgb) :: cellStyle
                                in
-                                  div [ style [ ("float", "left")
-                                              , ("position", "relative")
-                                              , ("height", px cellHeight)
-                                              , ("width", px cellWidth)
-                                              , ("background-color", cellRgb model colIndex rowIndex)
-                                              ]
-                                      ]
-                                      []
+                                 div [ style styles ] []
 
 buildRow : Model -> Int -> Html a
 buildRow model rowIndex = let
                             buildCell = (\colIndex -> cell model rowIndex colIndex)
                             cells = List.map buildCell [0..Snake.columnCount-1]
                           in
-                            div [ class "row"
-                                , style [ ("margin", "0")
-                                        , ("padding", "0")
-                                        ]
-                                ] cells
+                            div [ class "row", style rowStyle ] cells
 
 
 view : Model -> Html Msg
-view model = let gridWidth = Snake.columnCount * cellHeight
-             in
-               div [ class "grid container"
-                   , style [ ("width", px gridWidth)
-                           , ("padding", "0")
-                           ]
-                   ]
-                   (List.map (\rowIndex -> buildRow model rowIndex) [0..Snake.rowCount-1])
+view model = div [ class "grid container", style gridStyle ]
+                 (List.map (\rowIndex -> buildRow model rowIndex) [0..Snake.rowCount-1])
